a93ddc4168403539b326fb300270750fe1c04ced
Prasanth Reddy M <prasanthreddy.maddala@outlook.com>
2024-01-12 22:40:31 +0530

    Created a Template project Using Avalonia UI multi-platforms (Android, iOS, Desktop and Browser)

---
+++ MusicPlayer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace MusicPlayer.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ MusicPlayer/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MusicPlayer.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
