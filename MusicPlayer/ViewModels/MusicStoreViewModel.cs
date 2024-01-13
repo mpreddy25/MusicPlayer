8bf3061ac95246ea4fc6af0ca22f892d58cf72d2
Prasanth Reddy M <prasanthreddy.maddala@outlook.com>
2024-01-13 21:19:55 +0530

    Mock Search

--- MusicPlayer/ViewModels/MusicStoreViewModel.cs
+++ MusicPlayer/ViewModels/MusicStoreViewModel.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MusicPlayer.ViewModels
 {
-    public class MusicStoreViewModel
+    public class MusicStoreViewModel : ViewModelBase
     {
+        private string? _searchText;
+        private bool _isBusy;
+
+        public string? SearchText
+        {
+            get => _searchText;
+            set => this.RaiseAndSetIfChanged(ref _searchText, value);
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+        }
+
+        private AlbumViewModel? _selectedAlbum;
+
+        public ObservableCollection<AlbumViewModel> SearchResults { get; } = new();
+
+        public AlbumViewModel? SelectedAlbum
+        {
+            get => _selectedAlbum;
+            set => this.RaiseAndSetIfChanged(ref _selectedAlbum, value);
+        }
+
+        public MusicStoreViewModel()
+        {
+            SearchResults.Add(new AlbumViewModel());
+            SearchResults.Add(new AlbumViewModel());
+            SearchResults.Add(new AlbumViewModel());
+        }
     }
 }
